c335110b96c45ba597a9c1284ea136847fa40fab
Aayush Chauhan <149661682+aayush010904@users.noreply.github.com>
2024-12-25 12:49:55 +0530

    minor fixes

--- Fin-bot.py
+++ Fin-bot.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import random
 import time
-# import phidata
 from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.run.response import RunResponse
@@ -12,11 +11,11 @@ from phi.utils.pprint import pprint_run_response
 load_dotenv()
 model_name = "llama-3.1-70b-versatile"
 agent = Agent(
-    model = Groq(id = "llama-3.1-70b-versatile"),
-    tools = [YFinanceTools(stock_price = True, analyst_recommendations = True, stock_fundamentals = True)],
-    show_tool_calls = True,
-    markdown = True,
-    instructions = ["Use tables to display data"],
+    model=Groq(id="llama-3.1-70b-versatile"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
+    show_tool_calls=True,
+    markdown=True,
+    instructions=["Use tables to display data"],
     # debug_mode = True
 )
 
@@ -24,13 +23,11 @@ st.title("Fin-Bot")
 st.subheader(f"Your finance assistant")
 st.write("This bot can assist you with finance related queries*")
 st.write("Made with ❤️ by Aayush Chauhan | © 2024")
-st.markdown(f"####### *accuracy of answers is subject to model accuracy.")
-    
+
 if st.button('Instructions'):
     st.markdown(f"- Use Stocks symbols for calling stocks, eg. TSLA for Tesla and NVDA for NVIDIA")
     st.markdown(f"- Ask Questions related to Stock Prices, Analyst recommendations, and News ")
-
-
+    st.markdown(f"- *answers are subject to model accuracy (current model is {model_name})")
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -41,10 +38,11 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 prompt = st.chat_input("Type your query")
 
-response: RunResponse = agent.run(prompt)
+
 
 # React to user input
-if prompt :
+if prompt:
+    response: RunResponse = agent.run(prompt)
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
